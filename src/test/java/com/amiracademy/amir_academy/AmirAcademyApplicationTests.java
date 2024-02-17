29329449dea44c12ab800232189acfe9147d953b
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-02-17 11:38:26 +0100

    first commit

---
+++ src/test/java/com/amiracademy/amir_academy/AmirAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amiracademy.amir_academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmirAcademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
